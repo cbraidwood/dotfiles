3033f3982bb734ca18db12a891d5fdc6c457ee3c
oryzomys <user142938@protonmail.ch>
2023-06-24 00:44:23 +0100

    added basic elfeed functionality to emacs conf

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -10,10 +10,8 @@
  '(custom-safe-themes
    '("d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "986cdc701d133f7c6b596f06ab5c847bebdd53eb6bc5992e90446d2ddff2ad9e" "71ac1434a07579da9b1ec1dd1a2b9cfa3182523d750678b68db6c25749fb6494" "53585ce64a33d02c31284cd7c2a624f379d232b27c4c56c6d822eff5d3ba7625" "1fab98300b100a19010734a14c4bf9b6712ffc8b9e1d7eca35f837adeeabf740" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "6bdcff29f32f85a2d99f48377d6bfa362768e86189656f63adbf715ac5c1340b" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "4eb6fa2ee436e943b168a0cd8eab11afc0752aebb5d974bba2b2ddc8910fca8f" "70cfdd2e7beaf492d84dfd5f1955ca358afb0a279df6bd03240c2ce74a578e9e" "998975856274957564b0ab8f4219300bca12a0f553d41c1438bbca065f298a29" "83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" default))
  '(doc-view-continuous t)
- '(elfeed-feeds
-   '("www.youtube.com/feeds/videos.xml?channel_id=UCkf4VIqu3Acnfzuk3kRIFwA"))
  '(package-selected-packages
-   '(yasnippet-snippets company-box python-mode lsp-javacomp lsp-java lsp-mode modus-themes ligature haskell-mode proof-general ledger-mode math-preview latex-preview-pane org-roam elpher pov-mode company-irony company-c-headers projectile org-tempo zenburn-theme gruvbox-theme minimap auto-complete-auctex neotree pdf-tools auctex dired-launch magit use-package org evil-collection dashboard acme-theme)))
+   '(elfeed-org elfeed yasnippet-snippets company-box python-mode lsp-javacomp lsp-java lsp-mode modus-themes ligature haskell-mode proof-general ledger-mode math-preview latex-preview-pane org-roam elpher pov-mode company-irony company-c-headers projectile org-tempo zenburn-theme gruvbox-theme minimap auto-complete-auctex neotree pdf-tools auctex dired-launch magit use-package org evil-collection dashboard acme-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
